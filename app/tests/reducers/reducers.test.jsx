b7fdf4f912578129702f141fca81a684f5b8eaf1
Zach Bullock <bullock.zd@gmail.com>
2017-03-09 01:18:26 -0600

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,57 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 0
+      };
+
+      var testState = [
+        {
+          id: 0,
+          text: 'hello',
+          completed: false,
+          createdAt: undefined,
+          completedAt: undefined
+        }, {
+          id: 1,
+          text: 'hi yo',
+          completed: false,
+          createdAt: undefined,
+          completedAt: undefined
+        }, {
+          id: 2,
+          text: 'gigger gabber',
+          completed: false,
+          createdAt: undefined,
+          completedAt: undefined
+        }, {
+          id: 3,
+          text: 'Waka Flaka',
+          completed: false,
+          createdAt: undefined,
+          completedAt: undefined
+        }
+      ];
+
+      var res = reducers.todosReducer(df(testState), df(action));
+
+      expect(res.length).toBe(4);
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toBeA('number');
+    });
+  });
 });
